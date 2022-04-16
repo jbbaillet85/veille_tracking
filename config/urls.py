a0d34c9411e8d54cf4bca53a17cef9be9c5c99e9
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-04-16 11:37:07 +0200

    creation templates base et homepage

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="base.html")),
+    path('', TemplateView.as_view(template_name="homepage/homepage.html")),
     path('user/', include('app_user.urls')),
     path('books/', include('app_books.urls')),
 ]
