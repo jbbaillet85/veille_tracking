90a4e948c88504fe2b135d8b618a8157c9ac4d49
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-07-14 16:13:04 +0200

    build: add psycopg2

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -76,8 +76,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
