e7d54113479b7a8865d4776431e7fa4ddc9b9e51
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-10-17 12:15:53 +0200

    test: add creat_user and create_superuser

--- app_user/tests/tests_models_user.py
+++ app_user/tests/tests_models_user.py
@@ -8,10 +8,22 @@ class Test_User(TestCase):
     def setUp(self) -> None:
         self.email = "user@gmail.com"
         self.password = "password"
-        self.user = User.objects.create(
-            email=self.email, password=self.password)
         self.client = Client()
 
     @pytest.mark.django_db
-    def test_user_model(self):
+    def test_create_user_model(self):
+        self.user = User.objects.create_user(email=self.email, password=self.password)
         assert str(self.user) == self.email
+        self.assertEqual(self.user.email, "user@gmail.com")
+
+    @pytest.mark.django_db
+    def test_create_superuser_model(self):
+        self.superuser = User.objects.create_superuser(
+            email=self.email, password=self.password
+        )
+        assert str(self.superuser) == self.email
+        self.assertEqual(self.superuser.email, "user@gmail.com")
+
+    @pytest.mark.django_db
+    def test_create_user_if_not_email(self):
+        pass
