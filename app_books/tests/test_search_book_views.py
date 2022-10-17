c06b8bf6569d15f1ce331d4e133ee153ef3a2d60
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-10-17 17:13:28 +0200

    test: search_book_views

--- app_books/tests/test_search_book_views.py
+++ app_books/tests/test_search_book_views.py
@@ -1,26 +1,18 @@
 from unittest import TestCase
 import pytest
-
 from django.urls import reverse, resolve
 from django.test import Client
 from pytest_django.asserts import assertTemplateUsed
-from app_user.models import User
-from app_user.views import RegisterUserView
 
 
 class Test_Book(TestCase):
     def setUp(self):
-        self.user = User.objects.create(
-            email="test@gmail.com", password="123456")
-        self.register = RegisterUserView()
+        self.client = Client()
+        self.path = reverse("search_book")
+        self.response = self.client.get(self.path, kwargs={"keyword": "web"})
 
     @pytest.mark.django_db
     def test_search_book_view(self):
-        client = Client()
-        path = reverse("search_book")
-        response = client.get(
-            path, kwargs={"keyword": "web"}
-        )
-        self.assertEqual(response.status_code, 200)
-        assertTemplateUsed(response, "app_books/search_book.html")
-        self.assertEqual(resolve(path).view_name, "search_book")
+        self.assertEqual(self.response.status_code, 200)
+        assertTemplateUsed(self.response, "app_books/search_book.html")
+        self.assertEqual(resolve(self.path).view_name, "search_book")
