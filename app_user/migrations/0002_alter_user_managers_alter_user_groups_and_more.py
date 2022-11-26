13ee8b4c93664a1f4a38ca6735be755cf25920a6
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-11-26 17:07:54 +0100

    style: correction of PEP8 with Flake8

--- app_user/migrations/0002_alter_user_managers_alter_user_groups_and_more.py
+++ app_user/migrations/0002_alter_user_managers_alter_user_groups_and_more.py
@@ -31,7 +31,8 @@ class Migration(migrations.Migration):
             name='is_active',
             field=models.BooleanField(
                 default=True,
-                help_text='''Designates whether this user should be treated as active.
+                help_text='''Designates whether
+                this user should be treated as active.
                 Unselect this instead of deleting accounts.''',
                 verbose_name='active'),
         ),
