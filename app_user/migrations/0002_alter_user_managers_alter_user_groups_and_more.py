328ba98c9c2fc856d8d300778ed8ec00f92ef40f
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-11-26 09:51:45 +0100

    style: file migration app_user

--- app_user/migrations/0002_alter_user_managers_alter_user_groups_and_more.py
+++ app_user/migrations/0002_alter_user_managers_alter_user_groups_and_more.py
@@ -19,21 +19,38 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='groups',
-            field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups'),
+            field=models.ManyToManyField(
+                blank=True,
+                help_text='''The groups this user belongs to. A user will
+                get all permissions granted to each of their groups.''',
+                related_name='user_set', related_query_name='user',
+                to='auth.group', verbose_name='groups'),
         ),
         migrations.AlterField(
             model_name='user',
             name='is_active',
-            field=models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active'),
+            field=models.BooleanField(
+                default=True,
+                help_text='''Designates whether this user should be treated as active.
+                Unselect this instead of deleting accounts.''',
+                verbose_name='active'),
         ),
         migrations.AlterField(
             model_name='user',
             name='is_staff',
-            field=models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status'),
+            field=models.BooleanField(
+                default=False,
+                help_text='''Designates whether the user
+                can log into this admin site.''',
+                verbose_name='staff status'),
         ),
         migrations.AlterField(
             model_name='user',
             name='is_superuser',
-            field=models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status'),
+            field=models.BooleanField(
+                default=False,
+                help_text='''Designates that this user has all
+                permissions without explicitly assigning them.''',
+                verbose_name='superuser status'),
         ),
     ]
