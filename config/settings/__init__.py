30238ddfd12837f9a4cb3d67287ba9a264080863
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-12-20 10:13:13 +0100

    ci: repare test db

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -66,9 +66,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"), # noqa
-        "NAME": os.environ.get("SQL_DATABASE"),
-        "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "NAME": os.environ.get("SQL_DATABASE", "postgres"),
+        "USER": os.environ.get("SQL_USER", "postgres"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
         "HOST": os.environ.get("SQL_HOST"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
