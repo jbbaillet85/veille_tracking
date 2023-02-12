a0575f7b53a4b5c89404a6f1dc92e9b372faa8f3
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2023-02-12 18:55:57 +0100

    feat: add pagination for api in file settings

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -108,3 +108,8 @@ else:
     STATIC_ROOT = Path(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+    "rest_framework.pagination.PageNumberPagination",
+    'PAGE_SIZE': 2}
